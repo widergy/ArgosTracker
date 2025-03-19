6c5c7edeaf28d7d06f13dabaeff5e69c3ade8aa3
Diego Calero Costa <diego.calero@widergy.com>
2025-03-19 15:55:44 -0300

    feat: module plus request

--- lib/argos_tracker.rb
+++ lib/argos_tracker.rb
@@ -2,8 +2,50 @@
 
 require_relative 'argos_tracker/version'
 require 'argos_tracker/engine'
+require 'argos_tracker/config'
 
 module ArgosTracker
   class Error < StandardError; end
-  # Your code goes here...
-end
+
+  class BaseArgosTracker
+    attr_accessor :params, :response
+
+    @params = {}
+
+    def initialize(params = {})
+      @params = params
+      @response = nil
+    end
+
+    def merge_params(new_params)
+      @params.merge!(new_params)
+    end
+
+    def prepare_request(product, channel, utility_code, event_category, event_type)
+      options = ArgosTracker::Config.get_config(product)
+      {
+        url: options[:base_url],
+        headers: {
+          'api_key' => options[:api_key],
+          'Content-Type' => 'application/json'
+        },
+        body: body_request(options, product, channel, utility_code, event_category, event_type)
+      }
+    end
+
+    def body_request(options, product, channel, utility_code, event_category, event_type, flow_user_id = nil, flow_id = nil)
+      {
+        channel: channel, source: options[:source], product: options[:product],
+        utility_code: utility_code, timestamp: Time.now.to_i, event_category: event_category,
+        event_type: event_type, flow_user_id: flow_user_id, flow_id: flow_id, data: params
+      }
+    end
+
+    def send_request(http_verb, product, channel, utility_code, event_category, event_type)
+      options = prepare_request(product, channel, utility_code, event_category, event_type)
+      byebug
+      HTTParty.send(http_verb, options[:url], query: options[:query], body: options[:body],
+                    headers: options[:headers], timeout: options[:timeout])
+    end
+  end
+  
--- lib/argos_tracker/config.rb
+++ lib/argos_tracker/config.rb
@@ -3,12 +3,28 @@ module ArgosTracker
     @config = {}
 
     class << self
-      def set_config(product, api_key, source, product_name)
+      def set_config(product, api_key, source, product_name, enviroment = 'development')
+        url = get_url(enviroment)
+
         @config[product] = {
           api_key: api_key,
-          source: source,
-          product: product_name
+          base_url: url,
+          body: { source: source,
+                  product: product_name }
         }
+      end
+
+      def get_url(environment)
+        case environment
+        when 'development'
+          'https://argos.widergydev.com/v1'
+        when 'production'
+          'https://argos.widergy.com/v1'
+        when 'staging'
+          'https://argos.widergydev.com/v1'
+        else
+          'https://argos.widergydev.com/v1'
+        end
       end
 
       def get_config(product)
